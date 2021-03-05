26481a9d238023c2450da654cbdf6c165d3559fe
Amauri Junior <junior_18ribeiro@hotmail.com>
2021-03-05 19:46:28 -0400

    Start

---
+++ amauri-junior/components/intro.tsx
@@ -0,0 +1,17 @@
+import { CMS_NAME } from '../lib/constants'
+
+const Intro = () => {
+  return (
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        Amauri Junior
+      </h1>
+      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+        Compartilhando minha jornada de aprendizado do 0.5!
+        Não sei praticamente nada, e vou postar até aprender tudo o que puder! 
+      </h4>
+    </section>
+  )
+}
+
+export default Intro
